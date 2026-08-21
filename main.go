6a4daa05efc0fc25ffb96b9926de13f7dbd4140a
takaya-47 <local.s.s.47@gmail.com>
2026-08-21 09:48:56 +0900

    Ctrl+Cで停止できるようにしつつ、5秒に一回リクエストするように変更

--- main.go
+++ main.go
@@ -4,32 +4,48 @@ import (
 	"context"
 	"fmt"
 	"net/http"
+	"os"
+	"os/signal"
 	"time"
 )
 
 func main() {
+	ctx, stop := signal.NotifyContext(context.Background(), os.Interrupt)
+	defer stop()
+
 	client := http.Client{
 		Timeout: 10 * time.Second,
 	}
-
 	url := "https://www.city.uki.kumamoto.jp/"
-	req, err := http.NewRequestWithContext(context.Background(), http.MethodGet, url, nil)
-	if err != nil {
-		fmt.Println("error when creating request: ", err)
-		return
-	}
 
-	res, err := client.Do(req)
-	if err != nil {
-		fmt.Println("error when sending request: ", err)
-		return
-	}
+	ticker := time.NewTicker(5 * time.Second)
+	defer ticker.Stop()
 
-	defer res.Body.Close()
-	if res.StatusCode != http.StatusOK {
-		fmt.Println("error: status code is not OK: ", res.StatusCode)
-		return
-	}
+	for {
+		select {
+		case <-ctx.Done():
+			fmt.Println("Received interrupt signal, bye!")
+			return
+		case <-ticker.C:
+			req, err := http.NewRequestWithContext(ctx, http.MethodGet, url, nil)
+			if err != nil {
+				fmt.Println("error when creating request: ", err)
+				continue
+			}
 
-	fmt.Println("success: status code is OK", res.StatusCode)
+			res, err := client.Do(req)
+			if err != nil {
+				fmt.Println("error when sending request: ", err)
+				continue
+			}
+			defer res.Body.Close()
+
+			if res.StatusCode != http.StatusOK {
+				fmt.Println("error: status code is not OK: ", res.StatusCode)
+				continue
+			}
+
+			fmt.Println("success: status code is OK", res.StatusCode)
+		}
+	}
 }
