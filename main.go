5089a8be97de1a904010e886329f3e5d98307756
takaya-47 <local.s.s.47@gmail.com>
2026-09-03 16:34:42 +0900

    github.com/go-sql-driver/mysqlを導入しつつ、途中までリファクタ

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"context"
+	"database/sql"
 	"errors"
 	"fmt"
 	"log/slog"
@@ -11,16 +12,64 @@ import (
 	"sync"
 	"syscall"
 	"time"
+
+	_ "github.com/go-sql-driver/mysql"
 )
 
 func main() {
+	if err := run(); err != nil {
+		slog.LogAttrs(context.Background(), slog.LevelError, "failed to run", slog.String("error", err.Error()))
+		os.Exit(1)
+	}
+}
+
+// run はアプリケーションを開始する
+func run() error {
 	// 手動でCtrl+Cまたはコンテナ終了（SIGTERM）のシグナルが送られた場合にコンテキストをキャンセルする
 	ctx, stop := signal.NotifyContext(context.Background(), os.Interrupt, syscall.SIGTERM)
 	defer stop()
 
+	db, err := connectDB(ctx)
+	if err != nil {
+		return err
+	}
+	defer db.Close()
+
+	err = monitor(ctx)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+// connectDBはMySQLへの疎通を確認し、接続可能な場合はコネクションプールを返す
+func connectDB(ctx context.Context) (*sql.DB, error) {
+	// DSNの検証
+	db, err := sql.Open("mysql", os.Getenv("DB_DSN"))
+	if err != nil {
+		return nil, fmt.Errorf("dsn is invalid: %w", err)
+	}
+
+	db.SetConnMaxLifetime(3 * time.Minute) // MySQLサーバへの接続の寿命。経過後は接続を最初からやり直す。
+	db.SetMaxOpenConns(10)                 // コネクションプールに対して同時に開くことができる最大接続数
+	db.SetMaxIdleConns(10)                 // コネクションプールがアイドル状態で保持する接続の最大数
+
+	// 接続チェック
+	err = db.PingContext(ctx)
+	if err != nil {
+		return nil, fmt.Errorf("error when connecting to database: %w", err)
+	}
+	return db, nil
+}
+
+func monitor(ctx context.Context) error {
 	client := http.Client{
 		Timeout: 10 * time.Second,
 	}
+
+	// TODO: 監視結果をDBに保存する
+	// まず、リクエスト先のURLをDBから取得する
+	// 監視結果をmonitor_resultsテーブルに保存する
 	urls := []string{
 		"https://www.city.uki.kumamoto.jp/",
 		"https://www.town.hikawa.kumamoto.jp/",
@@ -38,7 +87,7 @@ func main() {
 		select {
 		case <-ctx.Done():
 			slog.LogAttrs(ctx, slog.LevelInfo, "monitoring stopped", slog.String("reason", ctx.Err().Error()))
-			return
+			return nil
 		case <-ticker.C:
 			var wg sync.WaitGroup
 			wg.Add(len(urls))
@@ -47,7 +96,7 @@ func main() {
 				go func(url string) {
 					defer wg.Done()
 
-					err := check(ctx, client, url)
+					err := check(ctx, &client, url)
 					if err != nil {
 						// エラーがキャンセルの場合はリクエスト自体はキャンセルされているが、正常終了として扱う
 						if errors.Is(err, context.Canceled) {
@@ -66,7 +115,7 @@ func main() {
 	}
 }
 
-func check(ctx context.Context, client http.Client, url string) error {
+func check(ctx context.Context, client *http.Client, url string) error {
 	req, err := http.NewRequestWithContext(ctx, http.MethodGet, url, nil)
 	if err != nil {
 		return fmt.Errorf("error when creating request: %w", err)
