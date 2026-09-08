fe6fdcda7f6ce55539b260d86ec73c2f15683aab
takaya-47 <local.s.s.47@gmail.com>
2026-09-08 13:15:50 +0900

    Revert "監視機能を./cmd/monitorに移動しつつ、DB接続をdatabaseパッケージに切り出した"
    This reverts commit 3e4d917dba9a75f3a9f75cb7b2d150b538f0b907.

--- go.mod
+++ go.mod
@@ -2,6 +2,7 @@ module github.com/takaya-47/lg-monitor
 
 go 1.26.3
 
-require github.com/go-sql-driver/mysql v1.10.0
-
-require filippo.io/edwards25519 v1.2.0 // indirect
+require (
+	filippo.io/edwards25519 v1.2.0 // indirect
+	github.com/go-sql-driver/mysql v1.10.0 // indirect
+)
--- main.go
+++ main.go
@@ -14,7 +14,6 @@ import (
 	"time"
 
 	_ "github.com/go-sql-driver/mysql"
-	database "github.com/takaya-47/lg-monitor/internal/db"
 )
 
 func main() {
@@ -30,7 +29,7 @@ func run() error {
 	ctx, stop := signal.NotifyContext(context.Background(), os.Interrupt, syscall.SIGTERM)
 	defer stop()
 
-	db, err := database.Connect(ctx)
+	db, err := connectDB(ctx)
 	if err != nil {
 		return err
 	}
@@ -41,6 +40,26 @@ func run() error {
 		return err
 	}
 	return nil
+}
+
+// connectDBはMySQLへの疎通を確認し、接続可能な場合はコネクションプールを返します。
+func connectDB(ctx context.Context) (*sql.DB, error) {
+	// DSNの検証
+	db, err := sql.Open("mysql", os.Getenv("DB_DSN"))
+	if err != nil {
+		return nil, fmt.Errorf("dsn is invalid: %w", err)
+	}
+
+	db.SetConnMaxLifetime(3 * time.Minute) // MySQLサーバへの接続の寿命。経過後は接続を最初からやり直す。
+	db.SetMaxOpenConns(10)                 // コネクションプールに対して同時に開くことができる最大接続数
+	db.SetMaxIdleConns(10)                 // コネクションプールがアイドル状態で保持する接続の最大数
+
+	// 接続チェック
+	err = db.PingContext(ctx)
+	if err != nil {
+		return nil, fmt.Errorf("error when connecting to database: %w", err)
+	}
+	return db, nil
 }
 
 // monitorは指定された間隔で監視を実行します。
